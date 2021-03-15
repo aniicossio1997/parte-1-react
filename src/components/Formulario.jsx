46ac08783c1b09e8c3ad2e7df6762e0f41566c24
ana cossio <anacossio7@gmail.com>
2021-03-14 22:19:02 -0300

    formulario basico con hooks

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,21 +3,58 @@ import React from 'react'
 const Formulario = () => {
   const [fruta, setFruta] = React.useState('')
   const [description, setDescription] =React.useState('')
+  const [lista, setLista] = React.useState([])
+
+
+  const guardarDatos = (e) => {
+    e.preventDefault()
+    if(!fruta.trim()){
+      console.log('El campo del nombre se encuentra vacio')
+      return
+    }
+    if(!description.trim()){
+      console.log("La description esta vacia")
+      return
+    }
+    console.log(`Datos procesados: ${fruta} ${description}` )
+    setLista([
+      ...lista,
+      {fruta: fruta, description: description}
+    ])
+    e.target.reset()
+    setFruta('')
+    setDescription('')
+    
+
+  }
   return (
     <div>
       <h1>Formulario</h1>
-      <form>
+      <form onSubmit={ guardarDatos}>
         <input 
         type="text"
         placeholder="Ingrese nombre"
         className="form-control mb-2"
+        onChange={ e => setFruta(e.target.value)}
+
         />
         <textarea
         placeholder="Ingrese description"
         className="form-control mb-2"
+        onChange={ e => setDescription(e.target.value)}
         ></textarea>
         <button className="btn btn-primary btn-block">Agregar</button>
       </form>
+
+      <ul>
+        {
+          lista.map((item, index) =>(
+            <li key={index}>
+              {item.fruta} - {item.description}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
