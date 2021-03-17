f6a4a5f6a37d021ab05c7e6513820238997803b3
ana cossio <anacossio7@gmail.com>
2021-03-17 09:05:54 -0300

    llamado de props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import Comentario from "./components/Comentario";
 import Saludo from "./components/Saludo";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container mt-5">
       <Saludo persona="juan" edad={30}/>
       <Saludo persona="antonio"/>
+      <hr/>
+      <h3>Cajita de comentario</h3>
+      <Comentario
+      urlImagen='https://picsum.photos/200'
+      persona='Juan'
+      texto='Lorem amet consectetur adipisicing elit. Sint possimus alias atque sapiente eius, ad excepturi odio obcaecati harum rem beatae, necessitatibus fugiat iste aspernatur facere, iusto sit? Nesciunt, aliquam.      '
+      />
+      <Comentario
+      urlImagen='https://picsum.photos/200'
+      persona='Hann'
+      texto='Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus alias atque sapiente eius, ad excepturi odio obcaecati harum rem beatae, necessitatibus fugiat iste aspernatur facere, iusto sit? Nesciunt, aliquam.      '
 
+      />
+      
     </div>
   );
 }
---
+++ src/components/Comentario.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Imagen from './Imagen'
+
+const Comentario = (props) => {
+  console.log(props)
+  return (
+    <div className="media mt-3">
+      <Imagen urlImagen={props.urlImagen}/>
+      <div className="media-body">
+        <h5 className="mt-0">
+           {props.persona}
+        </h5>
+        {props.texto}
+      </div>
+     
+    </div>
+  )
+}
+
+export default Comentario
